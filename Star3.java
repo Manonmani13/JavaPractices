7d54a38cc5ff5d82b276b052390a36c482674d42
manonmani <manonmani.pandukumar@gmail.com>
2021-09-06 22:41:23 +0530

    updated Star3.java

---
+++ Star3.java
@@ -0,0 +1,17 @@
+package Monday;
+
+public class Star3 {
+	public static void main(String args[]) 
+	{
+	int i,j;
+	for(i=1;i<=5;i++) 
+	{
+		for(j=1;j<=5;j++) 
+		{
+			System.out.print(j);
+		}
+		System.out.println(" ");
+	}
+
+}
+}
