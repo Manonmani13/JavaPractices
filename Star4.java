ac41f7b92d5825672c09e2a34888e5e07e7e32b7
manonmani <manonmani.pandukumar@gmail.com>
2021-09-06 22:52:20 +0530

    updated Star4.java

---
+++ Star4.java
@@ -0,0 +1,17 @@
+package Monday;
+
+public class Star4 {
+	public static void main(String args[]) 
+	{
+	int i,j;
+	for(i=5;i>=1;i--) 
+	{
+		for(j=1;j<=5;j++) 
+		{
+			System.out.print(i);
+		}
+		System.out.println(" ");
+	}
+
+}
+}
