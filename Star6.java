e1f49849527be5d82c5c24fa6423b4786a8cd106
manonmani <manonmani.pandukumar@gmail.com>
2021-09-07 20:50:53 +0530

    updated Star6.java

---
+++ Star6.java
@@ -0,0 +1,15 @@
+package Tuesday;
+
+public class Star6 {
+public static void main(String args[]) {
+	int n=5,k=1;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++)
+		{
+			System.out.print("\t"+k);
+			k++;
+		}
+		System.out.println();
+	}
+}
+}
