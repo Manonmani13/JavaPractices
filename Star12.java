5d4f86da95899c1d0d74e01ec590d783db9971bf
manonmani <manonmani.pandukumar@gmail.com>
2021-09-07 21:25:29 +0530

    updated Star12.java

---
+++ Star12.java
@@ -0,0 +1,16 @@
+package Tuesday;
+
+public class Star12 {
+public static void main(String args[]) {
+	int n=5;int x;
+	for(int i=1;i<=n;i++) {
+		x=i;
+		for(int j=1;j<=n;j++) {
+			System.out.print("\t"+x);
+			x+=n;
+		}
+		System.out.println();
+		
+	}
+}
+}
