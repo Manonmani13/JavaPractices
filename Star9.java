3ae7cf4e874ae822602c88a1365d2d645446c8ba
manonmani <manonmani.pandukumar@gmail.com>
2021-09-07 21:10:00 +0530

    updated Star8.java

---
+++ Star9.java
@@ -0,0 +1,15 @@
+package Tuesday;
+
+public class Star9 {
+public static void main(String args[]) {
+	int n=5;
+	int x=2;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.print("\t"+x);
+			x+=2;
+		}
+		System.out.println();
+	}
+}
+}
