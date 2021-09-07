e754d547474f6ce79a3498f449967f6308386246
manonmani <manonmani.pandukumar@gmail.com>
2021-09-07 21:01:13 +0530

    updated Star7.java

---
+++ Star7.java
@@ -0,0 +1,14 @@
+package Tuesday;
+
+public class Star7 {
+public static void main(String args[]) {
+	int n=5,x=1;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.print(x+"\t");
+			x+=2;
+		}
+		System.out.println();
+	}
+}
+}
