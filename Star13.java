39e447b4c7ff956ddf0e73d9e15a3d5cd2ccebd6
manonmani <manonmani.pandukumar@gmail.com>
2021-09-08 21:42:57 +0530

    updated Star13.java

---
+++ Star13.java
@@ -0,0 +1,26 @@
+package wednesday;
+
+public class Star13 {
+public static void main(String args[]) {
+	int n=5;
+	int x,y;
+	for(int i=1;i<=n;i++)
+	{
+		x=i;
+		y=n-i+1;
+		for(int j=1;j<=n;j++)
+		{
+			if(j%2==1) {
+				System.out.print("\t"+x);		
+				}
+			else
+			{
+				System.out.print("\t"+y);
+			}
+			x=x+n;
+			y=y+n;
+		}
+		System.out.println();
+	}
+}
+}
