8d6c4afa291a240e86cab19210e750a37bfe62bd
manonmani <manonmani.pandukumar@gmail.com>
2021-09-08 22:06:00 +0530

    updated Star17.java

---
+++ Star17.java
@@ -0,0 +1,13 @@
+package wednesday;
+
+public class Star17 {
+public static void main(String args[]) {
+	int n=5;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.print((2*(i+j))-3+" ");
+		}
+		System.out.println();
+	}
+}
+}
