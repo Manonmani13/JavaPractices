4db6a71aa1762cb2630da643e73045fd4a7c5521
manonmani <manonmani.pandukumar@gmail.com>
2021-09-08 21:59:18 +0530

    updated Star16.java

---
+++ Star16.java
@@ -0,0 +1,13 @@
+package wednesday;
+
+public class Star16 {
+public static void main(String args[]) {
+	int n=5;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.print((i+j-1)+" "); 
+		}
+		System.out.println();
+	}
+}
+}
