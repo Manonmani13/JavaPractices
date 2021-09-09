4caf4ba9b516c24659c1605fafcdd37d21931c27
manonmani <manonmani.pandukumar@gmail.com>
2021-09-09 21:34:21 +0530

    updated Star29.java

---
+++ Star29.java
@@ -0,0 +1,13 @@
+package Thursday;
+
+public class Star29 {
+	public static void main(String args[]) {
+		char i,j;
+		for(i='E';i>='A';i--) {
+			for(j='E';j>='A';j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
