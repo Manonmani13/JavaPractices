dc8d864f8b7cb98f9491780a41457d5d6459dd30
manonmani <manonmani.pandukumar@gmail.com>
2021-09-09 21:27:32 +0530

    updated Star27.java

---
+++ Star27.java
@@ -0,0 +1,13 @@
+package Thursday;
+
+public class Star27 {
+public static void main(String args[]) {
+	char i,j;
+	for(i='A';i<='E';i++) {
+		for(j='A';j<='E';j++) {
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+}
+}
