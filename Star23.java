46715f9617d1d1cb7405b45f6b363009bea45cee
manonmani <manonmani.pandukumar@gmail.com>
2021-09-09 21:25:44 +0530

    updated Star23.java

---
+++ Star23.java
@@ -0,0 +1,15 @@
+package Thursday;
+
+public class Star23 {
+public static void main(String args[]) {
+	int n=5;
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			System.out.print((i%2)+" ");
+		}
+		System.out.println();
+	}
+}
+}
