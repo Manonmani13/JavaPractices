bd002ac0b54f98203916b3477f1f24de72be23f6
manonmani <manonmani.pandukumar@gmail.com>
2021-09-17 23:03:04 +0530

    updated Star33.java

---
+++ Star33.java
@@ -0,0 +1,23 @@
+package Friday;
+
+public class Star33 {
+	public static void main(String args[])
+	{
+		int x=0;
+		
+		int n=5;
+		
+		for(int i=0;i<n;i++)
+		{
+			x=i;
+			for(int j=1;j<=n;j++)
+			{
+				x+=n;
+				System.out.print((char)(x-n+65)+" ");
+				
+				}
+			System.out.println();
+		
+}
+}
+}
