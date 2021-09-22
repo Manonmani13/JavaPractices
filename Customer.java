90f71ed31473bf837c703c68b5717904eb7bfc5c
manonmani <manonmani.pandukumar@gmail.com>
2021-09-22 20:54:18 +0530

    updated Customer.java

---
+++ Customer.java
@@ -0,0 +1,10 @@
+public class Customer
+{
+public static void main(String args[])
+{
+Office officer=new Office();
+officer.doservice();
+System.out.println(Office.formfee);
+//officer.givesalary();
+}
+}
