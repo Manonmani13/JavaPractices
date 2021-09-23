3430574b10650cd05427aeb847b95ba096d1a0d0
manonmani <manonmani.pandukumar@gmail.com>
2021-09-23 16:52:51 +0530

    updated Pa.java

---
+++ Pa.java
@@ -0,0 +1,17 @@
+public class Pa
+{
+  int i = 100; 
+static int j = 200; 
+public  void watchNews(){
+System.out.println("Watching PT news"); 
+}
+public void takeRest()
+{
+System.out.println("Taking rest");
+}
+public static void walk()
+{
+System.out.println("Going for a walk");  
+}
+
+}
