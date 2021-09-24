917bc5a3bda184afc661de95b417f34037c965d4
manonmani <manonmani.pandukumar@gmail.com>
2021-09-24 13:24:33 +0530

    updated Bynch.java

---
+++ Bynch.java
@@ -0,0 +1,36 @@
+class Dyn
+{
+int i=1;
+	static String name="mano";
+Dyn()
+{
+	this.name=name;
+	System.out.println(name);
+}
+	Dyn(String name)
+{
+	this.name=name;
+	System.out.println(name);
+}
+}
+public class Bynch extends Dyn
+{
+int i=20;
+static String name="kirthu";
+Bynch()
+{
+	this.name=name;
+	System.out.println(name);
+}
+Bynch(String name)
+{
+	this.name=name;
+	System.out.println(name);
+}
+public static void main(String args[])
+{
+Dyn n=new Bynch("maha");
+System.out.println(n.i);
+System.out.println(n.name);
+}
+}
