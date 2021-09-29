a3e01e3f3e8c30110213b53d28817bdd70d071bf
manonmani <manonmani.pandukumar@gmail.com>
2021-09-29 16:37:02 +0530

    updated A.java

---
+++ A.java
@@ -0,0 +1,14 @@
+class A	
+{
+    A fun()
+    {
+        System.out.println("Base fun()");
+     
+    }
+	public static void main(String args[])
+    {
+       A base = new A();
+       base.fun();
+	   }
+}
+  
