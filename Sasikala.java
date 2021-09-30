faccd9a740f6cbfbe2e90fc1138a18562d83a971
manonmani <manonmani.pandukumar@gmail.com>
2021-09-30 17:55:31 +0530

    updated Sasikala.java

---
+++ Sasikala.java
@@ -0,0 +1,34 @@
+abstract class jailitha
+{
+public abstract void payfine();
+	abstract int i=10;
+	 jailitha()
+	{
+	System.out.println("hii");	
+	}
+public void laptop()
+{
+System.out.println("provide laptop");
+}
+public void bicycle()
+{
+System.out.println("provide bicycle");
+}
+public void ammamess()
+{
+System.out.println("provide food");
+}
+}
+
+class Sasikala extends jailitha
+{
+public static void main(String args[])
+{
+Sasikala s=new Sasikala();
+
+}
+public void payfine()
+{
+System.out.println("pay fine 100 crores");
+}
+}
