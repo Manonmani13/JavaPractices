b4df6935972c6048addf8d9983500b76d08bdfa2
manonmani <manonmani.pandukumar@gmail.com>
2021-10-02 16:48:42 +0530

    updated INdia.java

---
+++ SouthIndia.java
@@ -0,0 +1,16 @@
+ abstract class SouthIndia extends INdia
+{
+public SouthIndia(String primeMinister) {
+		super(primeMinister);
+		// TODO Auto-generated constructor stub
+	}
+public void cultivate()
+{
+System.out.println("Rice and Wheat cultivation");
+}
+public  void livingStyle()
+{
+System.out.println("Average development");
+}
+
+}
