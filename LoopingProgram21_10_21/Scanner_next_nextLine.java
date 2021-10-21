07c62c209c3694cfbeacc49de6a8225773a92d5c
manonmani <manonmani.pandukumar@gmail.com>
2021-10-21 22:28:50 +0530

    pattern programs

---
+++ LoopingProgram21_10_21/Scanner_next_nextLine.java
@@ -0,0 +1,28 @@
+package LoopingProgram21_10_21;
+
+import java.util.Scanner;
+
+public class Scanner_next_nextLine {
+	
+	public static void main(String[] args) {
+		System.out.println("enter values");
+		Scanner s=new Scanner(System.in);
+		 String c=s.nextLine();
+		 System.out.println(c+" ");
+		 
+}
+	void dis()
+	{
+		Scanner s1=new Scanner(System.in);
+		//char j=s1.next();
+		//System.out.println(j);
+	}
+}
+/*
+enter values
+Manonmani 
+manonmani
+Manonmani  manonmani
+
+
+*/
