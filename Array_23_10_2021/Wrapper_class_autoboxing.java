c0e543e13fcebfef17e364ba9ba736d720be6168
manonmani <manonmani.pandukumar@gmail.com>
2021-10-23 21:35:04 +0530

    boxing

---
+++ Array_23_10_2021/Wrapper_class_autoboxing.java
@@ -0,0 +1,13 @@
+package Array_23_10_2021;
+
+public class Wrapper_class_autoboxing {
+public static void main(String[] args) {
+	int a=10;
+	Integer b=a;
+	Integer i=Integer.valueOf(a);
+	System.out.println(a+"b values \t "+b+" i value\t"+i);
+}
+}
+/*
+10b values 	 10 i value	10
+*/
